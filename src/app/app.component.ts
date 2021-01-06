b3197b342b05280f316946b9a9495b97ca6949d7
Ismael Lopez <lomik33@hotmail.com>
2021-01-05 23:41:00 -0600

    Input, OutPut

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,61 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platzi-store';
+  items = ['ismael', 'paula', 'andres'];
+
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/images/camiseta.png',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/images/hoodie.png',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/images/mug.png',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/images/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/images/stickers1.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/images/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+
+  addItem(){
+    this.items.push('nuevo item' + this.items.length);
+  }
+
+  deleteItem (index: number){
+    this.items.splice(index, 1);
+  }
+  addCartDelegate = (e) =>{
+    console.log("delegate "+e);
+  }
 }
---
+++ src/app/components/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit, Output,EventEmitter } from '@angular/core';
+import { Product } from 'src/app/models/product.model';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+  @Input() product: Product;
+  constructor() { }
+  @Output() addCartClick: EventEmitter<any>= new EventEmitter();
+  ngOnInit() {
+  }
+
+  addCart=()=>{
+    console.log("Add Cart");
+    this.addCartClick.emit("emitir click de addcart" + this.product.title);
+  }
+
+}
